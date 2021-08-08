fc3fbff864f3f64a95f9c7bf8ae59e6fa007ffc7
Ali Ahmed <ali.ahmed@bracbank.com>
2021-08-08 20:55:09 +0600

    Project is created and basic dependancies added.

---
+++ app/src/main/java/com/aliahmed/sondertechnicaltest/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aliahmed.sondertechnicaltest.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aliahmed.sondertechnicaltest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
